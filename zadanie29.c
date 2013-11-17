5859556c5d51727643e52af4c1eac35e55b26c55
Karol Spica <karolloczek@gmail.com>
2013-11-17 12:41:26 +0100

    Create zadanie29.c

---
+++ zadanie29.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+  int tab[5];
+  int i;
+  int pomoc;
+  for (i = 0; i <= 4; i = i + 1){
+    printf("Podaj element tablic numer %d: \n", i);
+    scanf("%d", &tab[i]);
+  }
+  pomoc = tab[0];
+  tab[0]=tab[4];
+  tab[4]=pomoc;
+  for(i = 0; i <= 4; i++){
+    printf("tab[%d]= %d\n",i,tab[i]);
+	   }
+  return 0;
+}
